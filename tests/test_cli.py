855e1da7c7380c66ad273142a370d357d3052978
Scott Livingston <slivingston@cds.caltech.edu>
2015-10-12 10:21:59 -0700

    TEST: Reading version from command-line interface

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from StringIO import StringIO
+import sys
+
+import gr1py
+from gr1py import cli
+
+
+def test_version():
+    baitandswitch = StringIO()
+    stdout_cache = sys.stdout
+    sys.stdout = baitandswitch
+    assert cli.main(args=['gr1py', '-V']) == 0
+    sys.stdout = stdout_cache
+    (progname, ver) = baitandswitch.getvalue().split()
+    assert progname == 'gr1py'
+    assert gr1py.__version__ == ver
+    assert ('dev0' not in ver) and ('Unknown' not in ver)
